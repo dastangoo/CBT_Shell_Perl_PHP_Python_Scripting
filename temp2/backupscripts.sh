9b3f144aef6ad80c5340be55df1cdd5e7508cebf
dastangoo <hamed.dastangoo@gmail.com>
2017-10-30 15:00:00 +0330

    Backing Up Files in Bash

--- temp2/backupscripts.sh
+++ temp2/backupscripts.sh
@@ -4,20 +4,35 @@
 # Purpose: Used to backup files and/or directories locally and store remotely
 
 USER=superuser
-BACKUPDIR=../backup
-SCRIPTDIR=../temp2
-BACKUPFILE=scripts.backup.`date +%F`
+BACKUPDIR=../backup 
+SCRIPTDIR="../temp2 /etc"
+BACKUPFILE=scripts.backup.`date +%F`.xz
 BACKUPHOST=192.168.56.101
 REMOTEBACKUP=backup
-COUNT=`ls $BACKUPDIR/scripts.* | wc -l`
 THRESHOLD=7
-if [ $COUNT -le $THRESHOLD ]
-then
-	tar -czvf $BACKUPDIR/$BACKUPFILE $SCRIPTDIR > /dev/null
-	if [ $? != 0 ]; then echo Problems Creating Backup File; fi
-	scp $BACKUPDIR/$BACKUPFILE $USER@$BACKUPHOST:$REMOTEBACKUP
-	if [ $? != 0 ]; then echo Problems Copying Backup File to Backup Host; fi
-fi
 
+function checkbackupdir() {
+
+	if [ ! -e $BACKUPDIR ]
+	then
+		echo Creating Backup Directory because it doesn\'t exist
+		mkdir ../backup
+		COUNT=0
+#		exit 0
+	else
+		COUNT=`ls $BACKUPDIR/scripts.* | wc -l`
+	fi
+}
+function backup() {
+	if [ $COUNT -le $THRESHOLD ]
+	then
+		tar -cJvf $BACKUPDIR/$BACKUPFILE $SCRIPTDIR > /dev/null
+		if [ $? != 0 ]; then echo Problems Creating Backup File; fi
+		scp $BACKUPDIR/$BACKUPFILE $USER@$BACKUPHOST:$REMOTEBACKUP
+		if [ $? != 0 ]; then echo Problems Copying Backup File to Backup Host; fi
+	fi
+}
+checkbackupdir
+backup
 # END
    
